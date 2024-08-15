9eb861d89d4de86601c0119fbe3e169ce7afd332
esaibev <esaias.bevegard@gmail.com>
2024-08-15 13:44:16 +0200

    3.10

--- index.js
+++ index.js
@@ -101,7 +101,7 @@ app.post("/api/persons", (request, response) => {
   response.json(person);
 });
 
-const port = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);

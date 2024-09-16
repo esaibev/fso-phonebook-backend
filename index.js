b2ebc428006b095c8d7e364e95f7be72839709b1
esaibev <esaias.bevegard@gmail.com>
2024-09-16 14:45:20 +0200

    Add validation

--- index.js
+++ index.js
@@ -80,7 +80,7 @@ app.delete("/api/persons/:id", (request, response, next) => {
     .catch((error) => next(error));
 });
 
-app.post("/api/persons", (request, response) => {
+app.post("/api/persons", (request, response, next) => {
   const body = request.body;
 
   if (!body.name) {
@@ -104,10 +104,13 @@ app.post("/api/persons", (request, response) => {
     number: body.number,
   });
 
-  person.save().then((savedPerson) => {
-    console.log(`SavedPerson: ${savedPerson}`);
-    response.json(savedPerson);
-  });
+  person
+    .save()
+    .then((newPerson) => {
+      console.log(`NewPerson: ${newPerson}`);
+      response.json(newPerson);
+    })
+    .catch((error) => next(error));
 });
 
 app.put("/api/persons/:id", (request, response, next) => {
@@ -125,7 +128,11 @@ app.put("/api/persons/:id", (request, response, next) => {
     number: body.number,
   };
 
-  Person.findByIdAndUpdate(request.params.id, updatedPerson, { new: true })
+  Person.findByIdAndUpdate(request.params.id, updatedPerson, {
+    new: true,
+    runValidators: true,
+    context: "query",
+  })
     .then((updatedPerson) => response.json(updatedPerson))
     .catch((error) => next(error));
 });
@@ -139,6 +146,8 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === "CastError") {
     return response.status(400).send({ error: "Malformatted id" });
+  } else if (error.name === "ValidationError") {
+    return response.status(400).json({ error: error.message });
   }
 
   next(error);
